a0bad69ac3a68900e2973ce3dcf79f45b8084269
lily092905 <209410629@gms.tku.edu.tw>
2023-02-07 00:04:56 +0800

    initial

---
+++ IAmTimCorey/ExceptionsDemo/ExceptionsDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExceptionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+            int[] testcase = new int[] { 0, 2, 1, 4, 1 };
+            for(int i =0; i<testcase.Length; i++)
+            {
+                try
+                {
+                    int result = demo.GrandparentMethod(testcase[i]);
+                    Console.WriteLine($"The value at the given position is {result}");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IAmTimCorey/ExceptionsDemo/ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandparentMethod(int position)
+        {
+            return ParentMethond(position);
+        }
+        public int ParentMethond(int position)
+        {
+            return GetNumber(position);
+        }
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            //try
+            //{
+                int[] numbers = new int[] { 1, 3, 5, 7 };
+                output = numbers[position];
+                if(output == 3)
+                {
+                    throw new Exception("test");
+                }
+            //}catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            return output;
+        }
+    }
+}
